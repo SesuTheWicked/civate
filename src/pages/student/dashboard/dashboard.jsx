b55cce58d54cb1cceb1c2964cd272726818b9842
Sesugh Asa <137288933+SesuTheWicked@users.noreply.github.com>
2023-10-16 15:02:26 +0100

    again again

--- src/pages/student/dashboard/dashboard.jsx
+++ src/pages/student/dashboard/dashboard.jsx
@@ -4,8 +4,15 @@ import Footer from '../../../components/containers/footer/footer'
 import IdSlide from '../../../components/containers/id_slide/IdSlide'
 import trois from '../../../assets/trois.svg'
 import cinq from '../../../assets/cinq.svg'
+import yuu from '../../../assets/yuu.svg'
+import yut from '../../../assets/yut.svg'
 import quatre from '../../../assets/quatre.svg'
 import seize from '../../../assets/seize.svg'
+import reading from '../../../assets/reading.png'
+import reading2 from '../../../assets/reading2.png'
+import read_photo from '../../../assets/read_photo.png'
+import read_photo2 from '../../../assets/read_photo2.png'
+
 
 
 
@@ -24,14 +31,14 @@ const StudentDashboard = () => {
      <div className=''>
       <div className='bold_dashboard'><img src={trois} alt="img" className='img_set'/>My Dashboard</div>
       <br />
-      <div className='mon'><a href="/" className='my_test'>My Tests</a>   <a href="/" className=''>view all Tests</a></div>
+      <div className='mon'><a href="/" className='my_test'>My Tests</a>   <a href="/" className='view'>view all Tests</a></div>
       <div className='block_functions'>
 
-        <div className=''><a href="/" className='reg_find'>Register/Find Test Centers</a></div>
-        <div className=''><img src="" alt="" />Upcoming Tests</div>
-        <div className=''>No Test For now!</div>
-        <div className=''><img src="" alt="" />Scores</div>
-        <div><a href="/" className=''>Press to see the list</a></div>
+        <div className='green_white'><a href="/" className='reg_find'>Register/Find Test Centers</a></div>
+        <div className='green_white'><span className='fill_text'><img src={yuu} alt="img" className='img_jk' />Upcoming Tests</span></div>
+        <div className='green_white'> <span className='reg_find2'>No Test For now!</span></div>
+        <div className='green_white'><span className='fill_text'><img src={yut} alt="" className='img_jk'/>Scores</span></div>
+        <div className='green_white'><a href="/" className='reg_find2'>Press to see the list</a></div>
       </div>
 
      </div>
@@ -59,8 +66,32 @@ Test Center Procedures & <br /> Regulations</p>
 
 
      </div>
+     <div className='foto_flex'>
+
+
+  <div className='green_girl'>
+    <div><img src={read_photo2} alt="img" /></div>
+    <div className='green_girl_div'><img src={reading2} alt="img" /></div>
+    <div className='grow_cicate'>Grow With <br />Cicate</div>
+    </div>
+
+
+  <div className='green_boy'>
+    <div className='gy'><img src={reading} alt="img" /></div>
+    <div className='green_boy_div'><img src={read_photo} alt="img" /></div>
+    <div className='cicate_move'>
+      <span className=''>Cicate</span> <br />
+      <span className='like_pro'> Take Test <br />
+        Like Pro!</span>
+      <br />
+      <span className='like_pro1'>Test yourself!</span>
       </div>
-      <Footer/>
+    </div>
+
+
+</div>
+      </div>
+    
     </div>
   )
 }
